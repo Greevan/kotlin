3a46473409281840f770277936de26f95de5137b
Mr.Blue <dhanush@localhost.localdomain>
2022-07-13 14:22:47 +0530

    Nooby kodes

---
+++ Youtube/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(){
+    var user=User()
+    user.firstName="DhasaGreevan"
+    user.lastName="Chandrasekar"
+    user.printFullName()
+    user.firstNameLength()
+    }
---
+++ Youtube/src/main/kotlin/User.kt
@@ -0,0 +1,12 @@
+class User{
+    var firstName: String=""
+    var lastName: String=""
+
+    fun printFullName(){
+        println("First Name =$firstName\nLast Name=$lastName")
+         }
+    fun firstNameLength(){
+        println(firstName.length)
+        println(lastName.length)
+    }
+}
